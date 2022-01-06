1b557213310a8638d062d8562902b9cbdd1d9d20
RostikKharkiv <dmitriy300500@mail.ru>
2022-01-06 14:40:46 +0300

    Added task 1.1.4

---
+++ Task 1/Task 1.1/Task 1.1.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_1._1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = '*';
+            char space = ' ';
+            Console.Write("Enter N: ");
+            int n;
+            int.TryParse(Console.ReadLine(), out n);
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 1; j < i * 2; j += 2)
+                    Console.WriteLine(new string(space, n - j / 2) + new string(symbol, j));
+            }
+        }
+    }
+}
