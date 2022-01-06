cbcc0b8327b10f628079e4bdd18e908f0f0b0245
RostikKharkiv <dmitriy300500@mail.ru>
2022-01-06 14:31:44 +0300

    Added task 1.1.3

---
+++ Task 1/Task 1.1/Task 1.1.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_1._1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 10;
+            int spaces = N;
+
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j < spaces; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                spaces = spaces - 1;
+
+                for (int t = 1; t < i * 2; t++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
