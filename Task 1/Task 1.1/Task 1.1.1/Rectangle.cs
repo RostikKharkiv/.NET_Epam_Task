586e60579a9a1dd3f62639ae3ff20fb3cd033518
RostikKharkiv <dmitriy300500@mail.ru>
2022-01-06 13:35:37 +0300

    Added task 1.1.1

---
+++ Task 1/Task 1.1/Task 1.1.1/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1._1._1
+{
+    class Rectangle
+    {
+        private int firstSide;
+
+        private int secondSide;
+        public int FirstSide
+        {
+            get
+            {
+                return firstSide;
+            }
+            set
+            {
+                if (value > 0)
+                    firstSide = value;
+                else if (value < 1)
+                    Console.WriteLine("Invalid value");
+            }
+        }
+
+        public int SecondSide
+        {
+            get
+            {
+                return secondSide;
+            }
+            set
+            {
+                if (value > 0)
+                    secondSide = value;
+                else if (value < 0 || value == 0)
+                    Console.WriteLine("Invalid value");
+            }
+        }
+
+        public double Square => FirstSide * SecondSide;
+    }
+}
