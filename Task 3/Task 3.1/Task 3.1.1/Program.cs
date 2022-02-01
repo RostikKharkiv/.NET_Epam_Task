34350a5a9c9b8944ffd4f6fbd487cf96b58b672e
RostikKharkiv <dmitriy300500@mail.ru>
2022-02-01 22:24:02 +0400

    Added task 3.1.1

---
+++ Task 3/Task 3.1/Task 3.1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_3._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfPeoples;
+            int droppedOut;
+            Console.Write("Введите количество участников: ");
+            int.TryParse(Console.ReadLine(), out countOfPeoples);
+            Console.Write("Введите, какой по счёту человек будет вычеркнут каждый раунд: ");
+            int.TryParse(Console.ReadLine(), out droppedOut);
+            WeakestLink game = new WeakestLink(countOfPeoples, droppedOut);
+            game.GameStart();
+        }
+    }
+}
