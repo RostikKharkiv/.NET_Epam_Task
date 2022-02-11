023a36c8619d1426ab9e7c2475f7ffbbfcdc9460
RostikKharkiv <dmitriy300500@mail.ru>
2022-02-11 16:05:14 +0400

    Added task 3.3.1 P.S. Возможно я неправильно понял задание

---
+++ Task 3/Task 3.3/Task 3.3.1/ArrayExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3._3._1
+{
+    public static class ArrayExtension
+    {
+        public static int[] DoWithElements(this int[] array, Func<int, int> func)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = func(array[i]);
+            }
+
+            return array;
+        }
+
+        // сумма и среднее значение уже реализованы же
+
+        public static int Frequent(this int[] array)
+        {
+            if (array is null || array.Length == 0)
+            {
+                // если массив пустой то получим NullReferenceException
+            }
+
+            var res = array.GroupBy(x => x).OrderByDescending(x => x.Count()).FirstOrDefault();
+            
+            return res.Key;
+        }
+    }
+}
---
+++ Task 3/Task 3.3/Task 3.3.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_3._3._1 
+{ 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] mass = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] mass2 = { 4, 10 };
+
+            int Increase(int el) => el * 3;
+
+            mass.DoWithElements(Increase);
+
+            foreach (var item in mass)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(mass2.Frequent());
+            Console.WriteLine(mass2.Average());
+            Console.WriteLine(mass2.Sum());
+        }
+    }
+}
