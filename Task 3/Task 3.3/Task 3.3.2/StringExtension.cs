feed081b74b98d98635337f6f3d1ce9d6bda232b
RostikKharkiv <dmitriy300500@mail.ru>
2022-02-11 23:57:16 +0400

    Added task 3.3.2

---
+++ Task 3/Task 3.3/Task 3.3.2/StringExtension.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3._3._2
+{
+    public enum WordType
+    {
+        None = 0,
+        Russian = 1,
+        English = 2,
+        Number = 3,
+        Other = 4
+    }
+
+    public static class StringExtension
+    {
+        private static string russian = "АБВГДЕЁЖЗИЙКЛМНОПРСТУЙФЦЧШЩЬЪЫЭЮЯ" +
+            "абвгдеёжзийклмнопрстуфхцчшщьъыэюя";
+        private static string english = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+            "abcdefghijklmnopqrstuvwxyz";
+        private static string numbers = "0123456789";
+
+        private static WordType CheckWordType(string str)
+        {
+            if (str is null || str == String.Empty)
+            {
+                return WordType.None;
+            }
+
+            char firstSymbol = str[0];
+            int index = 1;
+
+            if (russian.Contains(firstSymbol))
+            {
+                while (index < str.Length)
+                {
+                    if (!russian.Contains(str[index]))
+                    {
+                        return WordType.Other;
+                    }
+
+                    index++;
+                }
+
+                return WordType.Russian;
+            }
+
+            if (english.Contains(firstSymbol))
+            {
+                while (index < str.Length)
+                {
+                    if (!english.Contains(str[index]))
+                    {
+                        return WordType.Other;
+                    }
+
+                    index++;
+                }
+
+                return WordType.English;
+            }
+
+            if (numbers.Contains(firstSymbol))
+            {
+                while (index < str.Length)
+                {
+                    if (!numbers.Contains(str[index]))
+                    {
+                        return WordType.Other;
+                    }
+
+                    index++;
+                }
+
+                return WordType.Number;
+            }
+
+            return WordType.Other;
+
+
+        }
+        public static WordType CheckLanguage(this string str)
+        {
+            string[] words = str.Split(new char[] { ' ', '.', '?', ',', '!' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (words.Length == 0)
+            {
+                return WordType.None;
+            }
+
+            if (words.Length == 1)
+            {
+                return CheckWordType(words[0]);
+            }
+
+
+            return WordType.Other;
+        }
+
+    }
+}
